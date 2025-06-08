6197aa0a1197409627f6707333db03f97f9fbb39
Giancyril <mijaresgiancyril@gmail.com>
2025-06-08 23:45:51 +0800

    Feat(sidebar): Implement live notice display

--- application/views/backend/sidebar.php
+++ application/views/backend/sidebar.php
@@ -127,6 +127,7 @@
                     </ul>
                 </li>
                 
+                <li> <a href="<?php echo base_url()?>notice/All_notice" ><i class="mdi mdi-clipboard"></i><span class="hide-menu">Notice <span class="hide-menu"></a></li>
                 <li> <a href="<?php echo base_url(); ?>settings/Settings" ><i class="mdi mdi-settings"></i><span class="hide-menu">Settings <span class="hide-menu"></a></li>
                 <?php } ?>
             </ul>
