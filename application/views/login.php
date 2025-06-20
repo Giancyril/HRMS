6b22acc39c7a4f55416ade3fa920f1ac2af67b58
Giancyril <mijaresgiancyril@gmail.com>
2025-06-20 19:46:49 +0800

    Revert "Refactor: Remove unused files"
    This reverts commit 8d4f168c1d153f314c45dee4d5537a65535d2c0e.

--- application/views/login.php
+++ application/views/login.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Optima HR</title>
 
+    <script src="tailwind.config.js"></script>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
@@ -149,6 +151,8 @@
     <script src="<?php echo base_url(); ?>assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="<?php echo base_url(); ?>assets/js/script.js"></script>
 
 </body>
 
