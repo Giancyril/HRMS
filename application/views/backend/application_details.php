0fbbcee3b41c907f279333c4caf6e1c157934508
Giancyril <mijaresgiancyril@gmail.com>
2025-10-08 19:46:39 +0800

    feat: Enhance UI for Job & Application Details, and improve applications list data

--- application/views/backend/application_details.php
+++ application/views/backend/application_details.php
@@ -18,22 +18,98 @@
             <div class="col-12">
                 <div class="card card-outline-info">
                     <div class="card-header">
-                        <h4 class="m-b-0 text-white">Application Details for <?php echo html_escape($application['first_name'] . ' ' . $application['last_name']); ?></h4>
+                        <h4 class="m-b-0 text-white">
+                            Application Details for: <?php echo html_escape($application['first_name'] . ' ' . $application['last_name']); ?>
+                            <span class="badge badge-info pull-right">
+                                Application ID: <?php echo html_escape($application['id']); ?>
+                            </span>
+                        </h4>
                     </div>
                     <div class="card-body">
+                        <?php
+                            $status = html_escape($application['status']);
+                            $badge_class = 'badge-secondary'; // Default
+                            if ($status == 'Pending') { $badge_class = 'badge-warning'; }
+                            else if ($status == 'Reviewed') { $badge_class = 'badge-info'; }
+                            else if ($status == 'Interview') { $badge_class = 'badge-primary'; }
+                            else if ($status == 'Hired') { $badge_class = 'badge-success'; }
+                            else if ($status == 'Rejected') { $badge_class = 'badge-danger'; }
+
+                            // Determine Applicant Type
+                            $is_internal = !empty($application['applicant_em_code']);
+                            $applicant_type = $is_internal ? 'Internal Employee' : 'External Applicant';
+                        ?>
+
                         <div class="row">
-                            <div class="col-md-6">
-                                <p><strong>Applicant ID:</strong> <?php echo html_escape($application['id']); ?></p>
-                                <p><strong>Applicant Name:</strong> <?php echo html_escape($application['first_name'] . ' ' . $application['last_name']); ?></p>
+                            <div class="col-md-6 border-right">
+                                <h5><i class="fa fa-user"></i> Applicant Information</h5>
+                                <hr>
+                                <p><strong>Name:</strong> <?php echo html_escape($application['first_name'] . ' ' . $application['last_name']); ?></p>
                                 <p><strong>Email:</strong> <?php echo html_escape($application['email']); ?></p>
                                 <p><strong>Phone:</strong> <?php echo html_escape($application['phone']); ?></p>
+                                <p>
+                                    <strong>Applicant Type:</strong> 
+                                    <span class="badge <?php echo $is_internal ? 'badge-success' : 'badge-info'; ?>">
+                                        <?php echo $applicant_type; ?>
+                                    </span>
+                                </p>
+                                
+                                <?php if ($is_internal): ?>
+                                    <p><strong>Employee PIN/Code:</strong> <?php echo html_escape($application['applicant_em_code']); ?></p>
+                                <?php endif; ?>
                             </div>
+
                             <div class="col-md-6">
-                                <p><strong>Job Title:</strong> <?php echo html_escape($application['job_title']); ?></p>
-                                <p><strong>Job ID:</strong> <?php echo html_escape($application['job_id']); ?></p>
-                                <p><strong>Applied At:</strong> <?php echo html_escape(date('F j, Y', strtotime($application['applied_at']))); ?></p>
+                                <h5><i class="fa fa-briefcase"></i> Job & Status Details</h5>
+                                <hr>
+                                <p><strong>Position Applied For:</strong> <?php echo html_escape($application['job_title']); ?></p>
+                                <p><strong>Job Posting ID:</strong> <?php echo html_escape($application['job_id']); ?></p>
+                                <p><strong>Date Applied:</strong> <?php echo html_escape(date('F j, Y h:i A', strtotime($application['applied_at']))); ?></p>
+                                <p>
+                                    <strong>Current Status:</strong> 
+                                    <span class="badge <?php echo $badge_class; ?>"><?php echo $status; ?></span>
+                                </p>
                             </div>
                         </div>
+                        
+                        <div class="clearfix m-t-20"></div>
+
+                        <div class="row m-t-20">
+                            <div class="col-md-6">
+                                <h5><i class="fa fa-cogs"></i> Application Actions</h5>
+                                <hr>
+                                <button type="button" class="btn btn-info"><i class="fa fa-download"></i> Download Resume</button>
+                                
+                                <div class="btn-group m-l-10">
+                                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Update Status
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Mark as Reviewed</a>
+                                        <a class="dropdown-item" href="#">Schedule Interview</a>
+                                        <a class="dropdown-item" href="#">Extend Offer</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item text-success" href="#">Mark as Hired</a>
+                                        <a class="dropdown-item text-danger" href="#">Mark as Rejected</a>
+                                    </div>
+                                </div>
+                                
+                                <a href="<?php echo base_url('recruitment/delete_application/' . $application['id']); ?>" 
+                                    class="btn btn-danger m-l-10 delete-application" 
+                                    title="Delete Application">
+                                    <i class="fa fa-trash-o"></i> Delete
+                                </a>
+                            </div>
+
+                            <div class="col-md-6 border-left">
+                                <h5><i class="fa fa-paperclip"></i> Notes & Attachments</h5>
+                                <hr>
+                                <p class="text-muted">
+                                    * File upload/notes section can be added here. (e.g., Cyril Naig's Resume.pdf)
+                                </p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
--- application/views/backend/job_details.php
+++ application/views/backend/job_details.php
@@ -16,23 +16,65 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="card">
+                <div class="card card-outline-info">
+                    
+                    <div class="card-header">
+                        <h4 class="m-t-0 m-b-0 text-white" style="float: left; line-height: 30px;">
+                            <?php echo html_escape($job_details->job_title); ?>
+                        </h4>
+
+                        <div class="pull-right">
+                            <a href="<?php echo site_url('recruitment/applications?job_id=' . $job_details->job_id); ?>" class="btn btn-sm btn-info m-r-5">
+                                View Applications
+                            </a>
+                            <a href="#" class="btn btn-sm btn-info m-r-5">
+                                <i class="fa fa-pencil"></i> Edit Job
+                            </a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    
                     <div class="card-body">
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <h2 class="text-themecolor"><?php echo html_escape($job_details->job_title); ?></h2>
-                                <p class="text-muted"><i class="fa fa-clock"></i> Posted: <?php echo html_escape(date('F j, Y', strtotime($job_details->posted_at))); ?></p>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="m-0">Job Description</h5>
-                                <p><?php echo nl2br(html_escape($job_details->job_description)); ?></p>
-
-                                <hr class="my-4">
-
-                                <h5 class="m-0">Requirements</h5>
-                                <p><?php echo nl2br(html_escape($job_details->requirements)); ?></p>
+                        
+                        <div class="row m-b-20">
+                            <div class="col-md-12">
+                                <p class="text-muted">
+                                    <i class="fa fa-tag m-r-5"></i> Job ID: <strong><?php echo html_escape($job_details->job_id); ?></strong>
+                                    <span class="m-l-20">|</span>
+                                    <i class="fa fa-calendar m-r-5"></i> Posted Date: 
+                                    <strong><?php echo html_escape(date('F j, Y', strtotime($job_details->posted_at))); ?></strong>
+                                    <span class="m-l-20">|</span> 
+                                    <i class="fa fa-check-circle m-r-5"></i> Status: 
+                                    <span class="badge badge-success">Active</span> 
+                                </p>
+                                <hr>
                             </div>
                         </div>
+
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="card card-hover">
+                                    <div class="card-header bg-info text-white">
+                                        <h5 class="m-b-0"><i class="fa fa-info-circle m-r-5"></i> Job Description</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <p style="white-space: pre-wrap;"><?php echo html_escape($job_details->job_description); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-5">
+                                <div class="card card-hover">
+                                    <div class="card-header bg-warning text-white">
+                                        <h5 class="m-b-0"><i class="fa fa-list m-r-5"></i> Key Requirements</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <p style="white-space: pre-wrap;"><?php echo html_escape($job_details->requirements); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
